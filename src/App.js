b5addbf592ba4ae06d7605aede4e3e116c972042
nahuel meinardi <68476620+nmeinardi86@users.noreply.github.com>
2021-05-30 11:04:56 -0300

    agregado de bootsrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
  return <>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossorigin="anonymous"
+  />
    <NavBar />
    <div>Prueba</div>
    
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,47 @@
 
 import '../../App.css';
 
+
 function NavBar (){
  return <>
-    <nav>
-        <h1 className="tituloPrincipal">Panaderia Inmortales</h1>
-        <ul>
-            <a href=""> <p className="categoriasNavBar">Facturas</p></a>
-            <a href=""><p className="categoriasNavBar">Panes</p></a>
-            <a href=""><p className="categoriasNavBar">Pastas</p></a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Panaderia los inmortales</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <a className="nav-link" href="#navbar">Panes</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#navbar">Pastas</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#navbar">Sandwiches</a>
+            </li>
+            <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#navbar" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Facturas
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="#navbar">Bizcochos</a></li>
+                <li><a className="dropdown-item" href="#navbar">Factuas con crema</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#navbar">Facturas con membrillo</a></li>
+            </ul>
+            </li>
+            
         </ul>
-        
+        <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-primary" type="submit">Buscar Productos</button>
+        </form>
+        </div>
+       </div>
     </nav>
+    
     </>;
 }
 
